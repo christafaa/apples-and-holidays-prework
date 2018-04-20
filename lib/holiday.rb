5975ee3e3bc6c7352857c7f2a15f1dff3850721f
Chris Alberti <christopherjalberti@gmail.com>
2018-04-20 19:27:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts season
     holidays.each do |holiday, supplies|
+      puts "#{holiday}: #{supplies.join(", ")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
